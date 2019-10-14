fd3ea27e618edd891f8b749866042dfc02170b5b
Nicolas <nicolas.romero.f@usach.cl>
2019-10-13 21:57:10 -0300

    tune maxent y validacion cruzada

--- lab3.R
+++ lab3.R
@@ -1,10 +1,11 @@
 require('maxent')
 require('tm')
 require('SnowballC')
-
-path1 = "~/Documentos/2-2019/TMDA/tmdaLab3/data/amazon_cells_labelled.txt"
-path2 = "~/Documentos/2-2019/TMDA/tmdaLab3/data/imdb_labelled.txt"
-path3 = "~/Documentos/2-2019/TMDA/tmdaLab3/data/yelp_labelled.txt"
+pathroot = "/home/nicolas/Escritorio/USACH/Topicos/Taller de mineria de datos avanzada/tmdaLab3_MaxEnt/"
+#pathroot =~"/Documentos/2-2019/TMDA/tmdaLab3/"
+path1 = paste(pathroot,"data/amazon_cells_labelled.txt",sep="")
+path2 = paste(pathroot,"/data/imdb_labelled.txt",sep="")
+path3 = paste(pathroot,"/data/yelp_labelled.txt",sep="")
 #path = "~/Escritorio/USACH/Topicos/Taller de mineria de datos avanzada/tmdaLab2_randomForest/breast-cancer-wisconsin.data"
 
 data = read.delim(path1,header = FALSE)
@@ -20,7 +21,16 @@ colnames(dataAux) = colnames
 
 data = rbind(data,dataAux)
 
-corpus = Corpus(VectorSource(data$text))
+smp_size = floor(0.65*nrow(data))
+set.seed(12345)
+
+train_ind = sample(seq_len(nrow(data)),size = smp_size)
+
+data.train = data[train_ind,]
+data.test = data[-train_ind,]
+
+corpus = Corpus(VectorSource(data.train$text))
+
 
 summary(corpus)
 
@@ -45,3 +55,46 @@ corpus = tm_map(corpus,content_transformer(removeNumbers))
 for(i in 1:10) print(corpus[[i]]$content)
 
 matrix = DocumentTermMatrix(corpus)
+sparse <- as.compressed.matrix(matrix)
+
+data.me.t <- tune.maxent(sparse,data.train$class,nfold=3,showall=TRUE, verbose=TRUE)
+print(data.me.t)
+
+model<-maxent(sparse,data$class, l1_regularizer=0,6, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
+
+
+corpus.test = Corpus(VectorSource(data.test$text))
+
+
+summary(corpus.test)
+
+for(i in 1:10) print(corpus.test[[i]]$content)
+
+corpus.test = tm_map(corpus.test,content_transformer(removePunctuation))
+for(i in 1:10) print(corpus.test[[i]]$content)
+
+corpus.test = tm_map(corpus.test,content_transformer(tolower))
+for(i in 1:10) print(corpus[[i]]$content)
+
+corpus.test = tm_map(corpus.test,content_transformer(removeWords),stopwords("english"))
+for(i in 1:10) print(corpus[[i]]$content)
+
+corpus.test = tm_map(corpus.test,stemDocument)
+for(i in 1:10) print(corpus[[i]]$content)
+
+corpus.test = tm_map(corpus.test,stripWhitespace)
+for(i in 1:10) print(corpus[[i]]$content)
+
+corpus.test = tm_map(corpus.test,content_transformer(removeNumbers))
+for(i in 1:10) print(corpus.test[[i]]$content)
+
+matrix.test = DocumentTermMatrix(corpus.test)
+sparse.test <- as.compressed.matrix(matrix.test)
+
+results <- as.data.frame(predict(model,sparse.test))
+
+options(digits=15)
+results$predicted <- 0
+results$predicted[as.double(results$"0") > as.double(results$"1")] <- 0
+results$predicted[as.double(results$"0") < as.double(results$"1")] <- 1
+print(results)
