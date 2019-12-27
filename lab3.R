c7c2793d3ee576bff991cfb224e44e795948c59c
Nico <nicolas.romero.f@usach.cl>
2019-12-27 19:38:41 -0300

    bd separadas

--- lab3.R
+++ lab3.R
@@ -13,6 +13,18 @@ data = read.delim(path1,header = FALSE)
 colnames = c('text','class')
 colnames(data) = colnames
 
+data1 = read.delim(path1,header = FALSE)
+colnames1 = c('text','class')
+colnames(data1) = colnames1
+
+data2 = read.delim(path2,header = FALSE)
+colnames2 = c('text','class')
+colnames(data2) = colnames2
+
+data3 = read.delim(path3,header = FALSE)
+colnames3 = c('text','class')
+colnames(data3) = colnames3
+
 dataAux = read.delim(path2,header=FALSE)
 colnames(dataAux) = colnames
 
@@ -39,6 +51,15 @@ transform_corpus <- function(corpusF) {
   corpusF = tm_map(corpusF,content_transformer(removeNumbers))
   
   return(corpusF)
+}
+
+corpus_to_sparse <- function(corpus){
+  corpus.test = transform_corpus(corpus)
+  
+  matrix.test = DocumentTermMatrix(corpus.test)
+  sparse.test <- as.compressed.matrix(matrix.test)
+  
+  return(sparse.test)
 }
 
 
@@ -101,23 +122,15 @@ wordcloud(data.df.negative$word,data.df.negative$freq,min.freq=35,scale=c(2.0,0.
 
 
 
-smp_size = floor(0.65*nrow(data))
-set.seed(92347)
-
-train_ind = sample(seq_len(nrow(data)),size = smp_size)
-
-data.train = data[train_ind,]
-data.test = data[-train_ind,]
-
-corpus.train.np = Corpus(VectorSource(data.train$text))
+corpus.train.np = Corpus(VectorSource(data$text))
 
 matrix.train.np = DocumentTermMatrix(corpus.train.np)
 sparse.train.np <- as.compressed.matrix(matrix.train.np)
 
-data.me.t.np <- tune.maxent(sparse.train.np,data.train$class,nfold=3,showall=TRUE, verbose=TRUE)
+data.me.t.np <- tune.maxent(sparse.train.np,data$class,nfold=3,showall=TRUE, verbose=TRUE)
 print(data.me.t.np)
 
-model.np<-maxent(sparse.train.np,data.train$class, l2_regularizer=0.2, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
+model.np<-maxent(sparse.train.np,data$class, l2_regularizer=0.2, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
 
 
 corpus.train = transform_corpus(corpus.train.np)
@@ -125,13 +138,13 @@ corpus.train = transform_corpus(corpus.train.np)
 matrix.train = DocumentTermMatrix(corpus.train)
 sparse.train <- as.compressed.matrix(matrix.train)
 
-data.me.t <- tune.maxent(sparse.train,data.train$class,nfold=3,showall=TRUE, verbose=TRUE)
+data.me.t <- tune.maxent(sparse.train,data$class,nfold=3,showall=TRUE, verbose=TRUE)
 print(data.me.t)
 
-model<-maxent(sparse.train,data.train$class, l2_regularizer=0.2, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
+model<-maxent(sparse.train,data$class, l2_regularizer=0.2, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
 
 
-corpus.test.np = Corpus(VectorSource(data.test$text))
+corpus.test.np = Corpus(VectorSource(data$text))
 
 matrix.test.np = DocumentTermMatrix(corpus.test.np)
 sparse.test.np <- as.compressed.matrix(matrix.test.np)
@@ -140,17 +153,12 @@ results.np <- as.data.frame(predict(model.np,sparse.test.np))
 
 options(digits=15)
 results.np$relevant <- "1.relevante"
-results.np$relevant[which(results.np$label == 0)] <- "2.no relevante"
+results.np$relevant[which(results.np$label == 1)] <- "2.no relevante"
 results.np$recuperado <- "2.no recuperado"
-results.np$recuperado[which(data.test$class == results.np$label)] <- "1.recuperado"
-
-
-
+results.np$recuperado[which(data$class == results.np$label)] <- "1.recuperado"
 
 
 table(results.np$recuperado,results.np$relevant)
-
-
 
 corpus.test = transform_corpus(corpus.test.np)
 
@@ -161,13 +169,85 @@ results <- as.data.frame(predict(model,sparse.test))
 
 options(digits=15)
 results$relevant <- "1.relevante"
-results$relevant[which(results$label == 0)] <- "2.no relevante"
+results$relevant[which(results$label == 1)] <- "2.no relevante"
 results$recuperado <- "2.no recuperado"
-results$recuperado[which(data.test$class == results$label)] <- "1.recuperado"
+results$recuperado[which(data$class == results$label)] <- "1.recuperado"
+
+t = table(results$recuperado,results$relevant)
 
 
 
 
 
-table(results$recuperado,results$relevant)
+corpus.test1 = Corpus(VectorSource(data1$text))
+sparse.test1 <- corpus_to_sparse(corpus.test1)
 
+data.me.1<- tune.maxent(sparse.test1,data1$class,nfold=3,showall=TRUE, verbose=TRUE)
+print(data.me.1)
+
+model1<-maxent(sparse.test1,data1$class, l2_regularizer=0.4, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
+
+results1 <- as.data.frame(predict(model1,sparse.test1))
+
+options(digits=15)
+results1$relevant <- "1.relevante"
+results1$relevant[which(results1$label == 1)] <- "2.no relevante"
+results1$recuperado <- "2.no recuperado"
+results1$recuperado[which(data1$class == results1$label)] <- "1.recuperado"
+
+t1 = table(results1$recuperado,results1$relevant)
+
+
+
+
+
+
+
+corpus.test2 = Corpus(VectorSource(data2$text))
+sparse.test2 <- corpus_to_sparse(corpus.test2)
+
+data.me.2<- tune.maxent(sparse.test2,data2$class,nfold=3,showall=TRUE, verbose=TRUE)
+print(data.me.2)
+
+model2<-maxent(sparse.test2,data2$class,l2_regularizer = 0.6, use_sgd=FALSE, set_heldout=0, verbose=TRUE)
+
+results2 <- as.data.frame(predict(model2,sparse.test2))
+
+options(digits=15)
+results2$relevant <- "1.relevante"
+results2$relevant[which(results2$label == 1)] <- "2.no relevante"
+results2$recuperado <- "2.no recuperado"
+results2$recuperado[which(data2$class == results2$label)] <- "1.recuperado"
+
+t2 = table(results2$recuperado,results2$relevant)
+
+
+
+
+corpus.test3 = Corpus(VectorSource(data3$text))
+sparse.test3 <- corpus_to_sparse(corpus.test3)
+
+data.me.3<- tune.maxent(sparse.test3,data3$class,nfold=3,showall=TRUE, verbose=TRUE)
+print(data.me.3)
+
+model3<-maxent(sparse.test3,data3$class, use_sgd=TRUE, set_heldout=0, verbose=TRUE)
+
+results3 <- as.data.frame(predict(model3,sparse.test3))
+
+options(digits=15)
+results3$relevant <- "1.relevante"
+results3$relevant[which(results2$label == 1)] <- "2.no relevante"
+results3$recuperado <- "2.no recuperado"
+results3$recuperado[which(data3$class == results3$label)] <- "1.recuperado"
+
+t3 = table(results3$recuperado,results3$relevant)
+
+
+print("todos")
+print(t)
+print("amazon")
+print(t1)
+print("imdb")
+print(t2)
+print("yelp")
+print(t3)
